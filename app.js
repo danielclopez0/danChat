98cc99e9234370ce2cfa623db755460ae9a29c8c
danielclopez0 <danielclopez0@gmail.com>
2016-02-16 20:41:07 -0700

    added redis for persistance

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var redis = require('redis');
+var redisClient = redis.createClient();
+
+redisClient.on("error",function(err){
+	console.log("Error: " + err);
+});
 
 
 io.on('connection', function(client){
@@ -14,19 +20,43 @@ io.on('connection', function(client){
 		console.log('Client disconnected');
 	});
 
-	//establish client username
-	client.on('join',function(nickName){
-		client.userName = nickName;
-	});
 
 	//When a "messages" event comes in, log the message to the console and emit back to clients
 	client.on('messages',function(data){
 		var userName = client.userName;
-		var message = userName + ": " + data; 
 		console.log(data);
-		client.emit('messages',message);
-		client.broadcast.emit('messages',message);
+
+		//send to redis db client (10 most recent messages)
+		var storeMessage = function(userName, data){
+			var redisMessage = JSON.stringify({userName: userName, data: data});
+			redisClient.lpush("redisMessages",redisMessage, function(err,response){
+				redisClient.ltrim("redisMessages",0,9);
+			});
+		};
+
+		storeMessage(userName,data)
+
+
+		//emit to client
+		client.emit('messages',userName + ": " + data);
+		client.broadcast.emit('messages',userName + ": " + data);
 	});
+
+	//establish client username
+	client.on('join',function(nickName){
+		client.userName = nickName;
+
+		//retrieve most recent messages and send to client
+		redisClient.lrange("redisMessages",0,-1,function(err,redisMessages){
+			redisMessages = redisMessages.reverse();
+			redisMessages.forEach(function(message){
+				message = JSON.parse(message);
+				client.emit("messages",message.userName + ": " + message.data);
+			});
+		});
+	});
+
+	
 });
 
 app.get('/',function(req,res){
